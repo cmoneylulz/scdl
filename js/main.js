c9c098ea20d6b60652c2a25acdfd7f0047403758
cmoneylulz <element4emerica@gmail.com>
2014-09-06 20:28:21 +0400

    merge time

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-$.document.onLoad() = function()
+$.document.onload() = function()
 {
  	SC.initialize({
 	    client_id: "be76057de70ac578b8235ce2d29eae01",
 	    redirect_uri: "http://programmedby.me/scdl/callback.html",
 	});
-	
+
 	SC.connect(function() {
 	  	alert("hello its working");
     });
