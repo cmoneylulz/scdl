bbb2ca1f65b33803d7a9f37657b7dd1d3d7e67f0
cmoneylulz <element4emerica@gmail.com>
2014-09-06 12:45:03 -0400

    recovered main.js after epic merge fail. Take 2

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(window).onload(function()
+{
+ 	SC.initialize({
+	    client_id: "be76057de70ac578b8235ce2d29eae01",
+	    redirect_uri: "http://programmedby.me/scdl/callback.html",
+	});
+	
+	SC.connect(function() {
+	  	alert("hello its working");
+    });
+});
