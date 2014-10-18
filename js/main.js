74b7525722f50751878ee7af088b1e931a216455
cmoneylulz <element4emerica@gmail.com>
2014-10-18 05:32:10 +0400

    added new files, removed alert, now fetching user info

--- js/main.js
+++ js/main.js
@@ -3,8 +3,13 @@ $(document).ready(function () {
 	    client_id: "be76057de70ac578b8235ce2d29eae01",
 	    redirect_uri: "http://programmedby.me/scdl/callback.html",
 	});
-	
+
 	SC.connect(function() {
-	  	alert("hello its working");
+		var user_id;
+		SC.get("/me",  function (data) {
+			console.log(data);
+            user_id = data.id;
+		});
     });
+
 });
