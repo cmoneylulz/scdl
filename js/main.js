1fc48322bdcb82143d4a2e81e8dad921fa026f86
cmoneylulz <element4emerica@gmail.com>
2014-10-18 06:01:00 +0400

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
- 	SC.initialize({
-	    client_id: "be76057de70ac578b8235ce2d29eae01",
-	    redirect_uri: "http://programmedby.me/scdl/callback.html",
-	});
+    SC.initialize({
+        client_id: "be76057de70ac578b8235ce2d29eae01",
+        redirect_uri: "http://programmedby.me/scdl/callback.html",
+    });
 
-	SC.connect(function() {
-		var user_id;
-		SC.get("/me",  function (data) {
-			console.log(data);
+    SC.connect(function() {
+        var user_id;
+        SC.get("/me",  function (data) {
+            console.log(data);
             user_id = data.id;
-		});
+            $("#user_id").text(user_id);
+        });
     });
 
 });
