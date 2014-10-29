1ba1733406867953e4d7dcded0ed7f875455ae0b
cmoneylulz <element4emerica@gmail.com>
2014-10-29 20:46:18 +0300

    now actually showing the user id with jquery. I need to get better at callback hell

--- js/main.js
+++ js/main.js
@@ -1,16 +1,13 @@
-$(document).ready(function () {
-    SC.initialize({
-        client_id: "be76057de70ac578b8235ce2d29eae01",
-        redirect_uri: "http://programmedby.me/scdl/callback.html",
-    });
+SC.initialize({
+    client_id: "be76057de70ac578b8235ce2d29eae01",
+    redirect_uri: "http://programmedby.me/scdl/callback.html",
+});
 
+$(document).ready(function () {
     SC.connect(function() {
-        var user_id;
         SC.get("/me",  function (data) {
-            console.log(data);
-            user_id = data.id;
-            $("#user_id").text(user_id);
+            $('#user-id').html(data.id);
+            console.log(data.id);
         });
     });
-
 });
